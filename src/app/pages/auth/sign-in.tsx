4bdb9a83824a45c8c04c26cc440d1bfb060dd1fa
code4space <mauritius.william.wijaya1@gmail.com>
2023-11-15 13:16:25 +0700

    add showpassword when login

--- src/app/pages/auth/sign-in.tsx
+++ src/app/pages/auth/sign-in.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import {
   Card,
   CardHeader,
@@ -7,16 +7,18 @@ import {
   Button,
   Typography,
   Spinner,
+
 } from "@material-tailwind/react";
 import jwtDecode from "jwt-decode";
 import { login } from "@/app/services/auth";
 import { snackBar } from "@/utils/snackbar";
 import { getBackgroundColor } from "@/utils/general";
 
-export function SignIn () {
+export function SignIn() {
   const [username, setUseraname] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<string>("");
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
     setLoading(true);
@@ -24,7 +26,7 @@ export function SignIn () {
       username: username,
       password: password
     }
-    
+
     login(payload).then((data) => {
 
       if (data) {
@@ -45,6 +47,11 @@ export function SignIn () {
     })
   }
 
+  const toogleShowPassword = useCallback((e) => {
+    e.preventDefault()
+    setShowPassword(!showPassword)
+  }, [showPassword])
+
   return (
     <div className="relative min-h-screen w-full bg-gradient-to-tl from-green-900 via-green-300 to-green-900">
       <img
@@ -63,17 +70,31 @@ export function SignIn () {
           <CardBody className="flex flex-col gap-4">
             <form className="flex flex-col gap-4" onSubmit={handleLogin}>
               <Input required onChange={(e) => setUseraname(e.target.value)} color='green' type="input" label="Username" size="lg" crossOrigin={undefined} />
-              <Input required onChange={(e) => setPassword(e.target.value)} color='green' type="password" label="Password" size="lg" crossOrigin={undefined} />
+              <div className="input-login">
+                <Input required onChange={(e) => setPassword(e.target.value)} color='green' type={showPassword ? "input" : "password"} label="Password" size="lg" crossOrigin={undefined} />
+                <button onClick={(e) => toogleShowPassword(e)}>
+                  {showPassword ?
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                    </svg>
+                    :
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                  }
+                </button>
+              </div>
               <Typography color="blue-gray" className={'text-xs'}>
                 Forgot Password? {' '}
                 <a href={'reset-password'} color="green" className="hover:underline font-medium hover:text-green-500"> Reset Password</a>
               </Typography>
 
               <Button type={'submit'} disabled={loading || !password || !username} onClick={handleLogin} className={'flex justify-center mt-2'} variant="gradient" fullWidth color="green">
-                {loading ? <Spinner color="green" /> : 'Sign In' } 
+                {loading ? <Spinner color="green" /> : 'Sign In'}
               </Button>
             </form>
-            
+
           </CardBody>
         </Card>
       </div>
